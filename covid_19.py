e5873e4ad667dc08581208ddaebc9f4646d64cd7
Ahmet Hojamyradov <41162280+AhmetHojamyradov@users.noreply.github.com>
2020-04-11 01:52:49 +0300

    Update covid_19.py

--- covid_19.py
+++ covid_19.py
@@ -3,6 +3,10 @@ import covid19cases as covid
 import telebot
 bot = telebot.TeleBot("1268856054:AAHWunZt1XmHfG6pbwI_hQQEkjLF8HQA__A")
 
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Just type a country name to get up-to-date numbers on COVID-19 infected people in the chosen location.")
+
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
     country = covid.get_country_cases( message.text )
